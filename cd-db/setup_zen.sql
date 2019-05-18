fd66cf3c6cbec27c8eead760dafbed0d56641fab
Maimuna Ahmed <maimuna.ahmed8@mail.dcu.ie>
2019-05-18 15:45:48 +0100

    set up cp-leads-service database. run script for starting cp-leads-service

--- cd-db/setup_zen.sql
+++ cd-db/setup_zen.sql
@@ -1,4 +1,5 @@
 create user platform with superuser password 'QdYx3D5y';
+CREATE DATABASE "cp-leads-service" OWNER platform;
 CREATE DATABASE "cp-dojos-development" OWNER platform;
 CREATE DATABASE "cp-users-development" OWNER platform;
 CREATE DATABASE "cp-events-development" OWNER platform;
